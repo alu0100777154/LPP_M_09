bc447e89c40cde03e4a35a39a7d5c9cc947286b3
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2014-10-15 19:59:10 +0000

    Ambos productos realizados en multiply.rb y la division tambien

--- lib/complejo.rb
+++ lib/complejo.rb
@@ -2,6 +2,8 @@
 require lib/multiply
 
 Class Complejo
+    attr_reader :x, :y
+    attr_writer :x, :y
     def initialize(x,y)
         @x,@y = x,y
     end
--- lib/multiply.rb
+++ lib/multiply.rb
@@ -2,8 +2,17 @@
 require complejo
 def *(var)
     if var.is_a(Numeric)
-        @x,@y = var*@x,var*@y
+        Complejo.new(var*@x,var*@y)
     elsif var.is_a(Complejo)
-        @x,@y = ((var.x * @x) - var.y*@y),((var.y * @x) -(var.x * @y))
+        Complejo.new( ( (var.x * @x) - (var.y*@y) ), ( (var.y * @x) + (var.x * @y) ) )
     end
+end
+
+def /(var)
+   if var.is_a(Complejo)
+       Complejo.new( (( (var.x * @x) + (var.y*@y) / var.x**2 + var.y**2 )) , 
+                ( (var.x * @y) - (var.y * @x)  / var.x**2 + var.y**2  ) )
+    else
+        puts "Solo se pueden dividir complejos entre complejos"
+   end 
 end
