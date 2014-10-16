79df4420b7254a03ec33393d4c7ae6295db39704
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2014-10-16 13:36:03 +0000

    Pasado todo el codigo a complejo.rb

--- lib/complejo.rb
+++ lib/complejo.rb
@@ -1,5 +1,4 @@
 #Clase numero complejo
-require lib/multiply
 
 Class Complejo
     attr_reader :x, :y
@@ -7,3 +6,32 @@ Class Complejo
     def initialize(x,y)
         @x,@y = x,y
     end
+    
+    def *(var)
+        if var.is_a(Numeric)
+            Complejo.new(var*@x,var*@y)
+        elsif var.is_a(Complejo)
+            Complejo.new( ( (var.x * @x) - (var.y*@y) ), ( (var.y * @x) + (var.x * @y) ) )
+        end
+    end
+
+    def /(var)
+       if var.is_a(Complejo)
+           Complejo.new( ( ( (var.x * @x) + (var.y*@y) / (var.x**2 + var.y**2) ) ) , 
+                    ( (var.x * @y) - (var.y * @x)  / (var.x**2 + var.y**2)  ) )
+        else
+            puts "Solo se pueden dividir complejos entre complejos"
+       end 
+    end
+
+    def+(var)
+        if var.is_a(Complejo)
+            Complejo.new(var.x + @x , var.y + @y)
+        end
+    end
+    
+    def-(var)
+        if var.is_a(Complejo)
+             Complejo.new(@x - var.x , @y - var.y)
+        end
+    end  
