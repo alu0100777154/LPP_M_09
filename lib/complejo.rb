dcc2fccd126a245b2ce8913fbf0fa19ff72ba095
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2014-10-16 13:49:40 +0000

    Codigo de complejo.rb mas limpio y sin ifs innecesarios

--- lib/complejo.rb
+++ lib/complejo.rb
@@ -1,12 +1,11 @@
 #Clase numero complejo
 
-Class Complejo
+class Complejo
     attr_reader :x, :y
     attr_writer :x, :y
-    def initialize(x,y)
-        @x,@y = x,y
+    def initialize(x, y)
+        @x, @y = x, y
     end
-    
     def *(var)
         if var.is_a(Numeric)
             Complejo.new(var*@x,var*@y)
@@ -14,24 +13,14 @@ Class Complejo
             Complejo.new( ( (var.x * @x) - (var.y*@y) ), ( (var.y * @x) + (var.x * @y) ) )
         end
     end
-
     def /(var)
-       if var.is_a(Complejo)
-           Complejo.new( ( ( (var.x * @x) + (var.y*@y) / (var.x**2 + var.y**2) ) ) , 
-                    ( (var.x * @y) - (var.y * @x)  / (var.x**2 + var.y**2)  ) )
-        else
-            puts "Solo se pueden dividir complejos entre complejos"
-       end 
+           Complejo.new((((var.x * @x) + (var.y*@y) / (var.x**2 + var.y**2))),
+                    ((var.x * @y) - (var.y * @x)  / (var.x**2 + var.y**2)))
     end
-
     def+(var)
-        if var.is_a(Complejo)
             Complejo.new(var.x + @x , var.y + @y)
-        end
     end
-    
     def-(var)
-        if var.is_a(Complejo)
              Complejo.new(@x - var.x , @y - var.y)
-        end
     end  
+end
