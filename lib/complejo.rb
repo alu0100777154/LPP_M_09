94d1b15bdb5e3b52091edc612e17c30cb8ff68d5
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2014-10-16 13:51:48 +0000

    Codigo de complejo.rb mas limpio y sin ifs innecesarios v2

--- lib/complejo.rb
+++ lib/complejo.rb
@@ -8,7 +8,7 @@ class Complejo
     end
     def *(var)
         if var.is_a(Numeric)
-            Complejo.new(var*@x,var*@y)
+            Complejo.new(var*@x, var*@y)
         elsif var.is_a(Complejo)
             Complejo.new( ( (var.x * @x) - (var.y*@y) ), ( (var.y * @x) + (var.x * @y) ) )
         end
@@ -18,9 +18,9 @@ class Complejo
                     ((var.x * @y) - (var.y * @x)  / (var.x**2 + var.y**2)))
     end
     def+(var)
-            Complejo.new(var.x + @x , var.y + @y)
+            Complejo.new(var.x + @x, var.y + @y)
     end
     def-(var)
-             Complejo.new(@x - var.x , @y - var.y)
+             Complejo.new(@x - var.x, @y - var.y)
     end  
 end
